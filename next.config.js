3f66b488c5727c047c05b93ab55eeaa5bcda6818
Rodrigo Silva <rodrigo.goncalves2001r@gmail.com>
2023-11-27 13:35:23 -0300

    routed images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/portifolio' : '',
     output: "export",
     distDir: "dist",
     images: {
-        unoptimized: true,  
-    }
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
